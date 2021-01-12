6067192683e08c117e47eae8d7058635ced1e942
NariP <tto9208@naver.com>
2021-01-12 23:31:18 +0900

    "EFG-1:프로젝트 환경 설정 (CRA), gitignore 설정, node-sass, react-router-dom 라이브러리 설치"

---
+++ eg-front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './components/App.scss'
+
+function App() {
+  return (
+    <div className="App">
+        "hello World!"
+        <button>Button</button>
+    </div>
+  );
+}
+
+export default App;
