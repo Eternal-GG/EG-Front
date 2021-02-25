5ad7b5350ae1fed70723631d545992cc7f31a00c
NariP <tto9208@naver.com>
2021-02-26 03:11:47 +0900

    "EGF11:전적검색 심플 프로필 쪽 뷰 완성"

--- eg-front/src/components/MatchProfileSimple.js
+++ eg-front/src/components/MatchProfileSimple.js
@@ -8,7 +8,7 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles, commonObject);
 
-function MatchProfileSimple({gameNickname}) {
+function MatchProfileSimple({gameNickname, dropdown, dropdownHandler, season, seasonHandler}) {
     return(
         <section className={cx('MatchProfileSimple')}>
             <article>
@@ -20,16 +20,31 @@ function MatchProfileSimple({gameNickname}) {
                     <span className={cx('span-nickname')}>{gameNickname}</span>
                     <div>
                         <button className={cx('btn', 'yellow')}>전적 갱신</button>
-                        <button className={cx('btn', 'yellow')}>즐겨찾기</button>
+                        <button className={cx('btn', 'yellow')}>★ 즐겨찾기</button>
                     </div>
                     <span className={cx('span-time')}>최근 업데이트 : 27분 전</span>
                 </section>
                 <section>
-                    <span>일반<BiCaretDown/></span>
-                    <ul>
-                        <li><BiCheck/>일반</li>
-                        <li><BiCheck/>시즌1</li>
-                    </ul>
+                    <span onClick={dropdownHandler}>
+                        {season}
+                        {dropdown ? <BiCaretUp/> : <BiCaretDown/>}
+                    </span>
+                    {
+                        dropdown ?
+                            <ul>
+                                <li onClick={(e)=>{seasonHandler(e)}}>
+                                    {season === '일반' ? <BiCheck/> : null}
+                                    일반
+                                </li>
+                                <li onClick={(e)=>{seasonHandler(e)}}>
+                                    {season === '시즌1' ? <BiCheck/> : null}
+                                    시즌1
+                                </li>
+                            </ul>
+                        :
+                            null
+                    }
+
                 </section>
             </article>
         </section>
--- eg-front/src/pages/MatchHistoryPage.js
+++ eg-front/src/pages/MatchHistoryPage.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "../components/Header";
 import MatchProfileSimple from "../components/MatchProfileSimple";
 import MatchProfile from "../components/MatchProfile";
 
 function MatchHistoryPage({match, history}) {
     const {gameNickname} = match.params;
+    const [dropdown, setDropdown] = useState(false);
+    const [season, setSeason] = useState('일반');
+    // 0은 일반을 뜻함
+    const dropdownHandler = () => {
+        setDropdown(!dropdown);
+    };
+    const seasonHandler = (e) => {
+      setSeason(e.target.innerText);
+    };
     return(
         <>
             <Header history={history}/>
-            <MatchProfileSimple gameNickname={gameNickname}/>
+            <MatchProfileSimple
+                gameNickname={gameNickname}
+                dropdown={dropdown}
+                dropdownHandler={dropdownHandler}
+                season={season}
+                seasonHandler={seasonHandler}
+            />
             <MatchProfile />
             </>
     )
