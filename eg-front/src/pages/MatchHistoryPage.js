bc413cc603086a9b6c28e3e900fef6bda20b913e
NariP <tto9208@naver.com>
2021-03-01 23:21:20 +0900

    EGF-12: async await로 수정 dropdown css 버그 수정, 빌드시 소스맵 생성x

--- eg-front/src/pages/MatchHistoryPage.js
+++ eg-front/src/pages/MatchHistoryPage.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from "../components/Header";
 import MatchProfileSimple from "../components/MatchProfileSimple";
 import MatchProfile from "../components/MatchProfile";
+import {request} from '../utils/axios';
+import {DOMAIN, GAME_LIST_API} from "../Config";
 
 function MatchHistoryPage({match, history}) {
     const {gameNickname} = match.params;
@@ -14,6 +16,13 @@ function MatchHistoryPage({match, history}) {
     const seasonHandler = (e) => {
       setSeason(e.target.innerText);
     };
+    // useEffect(async()=>{
+    //     try {
+    //         const games = await request('get', `${GAME_LIST_API}/${gameNickname}/1`);
+    //     }catch (e) {
+    //         history.push('/error');
+    //     }
+    // });
     return(
         <>
             <Header history={history}/>
